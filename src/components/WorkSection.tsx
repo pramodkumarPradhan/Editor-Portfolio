097b7ea054a12d3e1d1ce792a8dc7a9102eb9c31
pramodkumarPradhan <pramodpradhan369@outlook.com>
2026-01-27 23:51:57 +0530

    data added

--- src/components/WorkSection.tsx
+++ src/components/WorkSection.tsx
@@ -5,6 +5,18 @@ import { Play, X } from "lucide-react";
 
 const demoVideos = [
   {
+    title: "Motion Graphic Showreel",
+    video: "https://res.cloudinary.com/dlcjyh6fb/video/upload/v1769537059/Motion_Graphic_Showreel_orxl6y.mp4",
+    poster: "https://res.cloudinary.com/dlcjyh6fb/image/upload/v1769537932/ae_thumbil_by4vmn.png",
+    category: "Motion Graphic",
+  },
+  {
+    title: "Video editing Showreel",
+    video: "https://res.cloudinary.com/dlcjyh6fb/video/upload/v1769536328/Video_editing_Showreel_vjss2g.mov",
+    poster: "https://res.cloudinary.com/dlcjyh6fb/image/upload/v1769538076/pr_fjbdwn.png",
+    category: "Showreel",
+  },
+  {
     title: "Wellness Insights",
     video: "https://res.cloudinary.com/dlcjyh6fb/video/upload/v1767591654/dpctor_kugbwy.mov",
     poster: "https://res.cloudinary.com/dlcjyh6fb/video/upload/v1767591654/dpctor_kugbwy.jpg",
@@ -57,18 +69,6 @@ const demoVideos = [
     video: "https://res.cloudinary.com/dlcjyh6fb/video/upload/v1767358227/kerala_kwzfzd.mov",
     poster: "https://res.cloudinary.com/dlcjyh6fb/video/upload/v1767358227/kerala_kwzfzd.jpg",
     category: "Travel",
-  },
-  {
-    title: "Motion Graphic Showreel",
-    video: "https://res.cloudinary.com/dlcjyh6fb/video/upload/v1769537059/Motion_Graphic_Showreel_orxl6y.mp4",
-    poster: "https://res.cloudinary.com/dlcjyh6fb/video/upload/v1769537059/Motion_Graphic_Showreel_orxl6y.jpg",
-    category: "Motion Graphic",
-  },
-  {
-    title: "Video editing Showreel",
-    video: "https://res.cloudinary.com/dlcjyh6fb/video/upload/v1769536328/Video_editing_Showreel_vjss2g.mov",
-    poster: "https://res.cloudinary.com/dlcjyh6fb/video/upload/v1769536328/Video_editing_Showreel_vjss2g.jpg",
-    category: "Showreel",
   }
 ];
 
